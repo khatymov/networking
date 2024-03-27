a1f3d7948e4561bd5ceca918b85ef4ac01f42b62
Rene <khatymovrenat@gmail.com>
2024-03-27 22:08:53 +0300

    #1 Add boost

--- main.cpp
+++ main.cpp
@@ -3,11 +3,24 @@
  */
 
 #include <iostream>
+#include <span>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
 
 using namespace std;
 
-int main()
-{
+int main(int argc, char* argv[]) {
+    const auto args = std::span(argv, size_t(argc));
+    if (args.size() < 2) {
+        cout << "Usage:\n";
+        cout << "If server:  ./networking 127.0.0.1\n";
+        cout << "If client:  ./networking 127.0.0.1 /path/to/file\n";
+    }
+
+    const string_view ip{args[1]};
+
+    boost::asio::io_context io_context;
 
     return 0;
 }
