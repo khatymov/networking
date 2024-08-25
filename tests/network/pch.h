a40140bc70a7bc5552da8992d9408ac8a50659a5
Renat <renat.khatymov@plc2.de>
2024-08-25 18:46:11 +0300

    add pre-complied header

---
+++ tests/network/pch.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <array>
+#include <cstdint>
+#include <cstdio>
+#include <filesystem>
+#include <fstream>
+#include <thread>
+#include <memory>
+#include <queue>
+#include <cryptopp/aes.h>
+#include <cryptopp/osrng.h>
+#include <cryptopp/modes.h>
+#include <openssl/sha.h>
+#include <boost/asio.hpp>
+
+#include "spdlog/spdlog.h"
--- tests/network/thread_safe_queue.h
+++ tests/network/thread_safe_queue.h
@@ -5,36 +5,80 @@
 #ifndef NETWORKING_THREAD_SAFE_QUEUE_H
 #define NETWORKING_THREAD_SAFE_QUEUE_H
 
-#include "my_packet.h"
+#ifndef QUEUE_DATA_NUM
+#define QUEUE_DATA_NUM 3
+#endif
 
-template <typename T= MyPacket<char>>
+#include "pch.h"
+
+namespace network {
+
+// entity of a class should be a smart pointer
+// please, do not create it on a stack
+
+template <typename T>
 class ThreadSafeQueue {
 public:
     // initially only one ThreadSafeQueue should create data that afterwords will be used in others
-    // ThreadSafeQueue. Only that object of ThreadSafeQueue should have isMainQueue=true
-    explicit ThreadSafeQueue(bool isMainQueue);
+    // ThreadSafeQueue. Only that object of ThreadSafeQueue should have isPrimaryQueue=true
+    /**
+    * @brief Constructor
+    */
+    explicit ThreadSafeQueue(bool isPrimaryQueue);
     //! \brief get data from queue
     std::unique_ptr<T> get();
     //! \brief set data to queue
-    void set(std::unique_ptr<T> data);
-private:
+    void set(std::unique_ptr<T>&& data);
+
+protected:
+    // queue with data allocated on a heap
+    std::queue<std::unique_ptr<T>> queue_;
+    // mutex to protect data race among different threads
+    std::mutex mutex_;
 };
 
-
 template <typename T>
-ThreadSafeQueue<T>::ThreadSafeQueue(bool isMainQueue) {
-
+ThreadSafeQueue<T>::ThreadSafeQueue(bool isPrimaryQueue) {
+    // we should create data once, in primary queue
+    if (isPrimaryQueue) {
+        for (int i = 0; i < QUEUE_DATA_NUM; ++i) {
+            // fill queue with empty data
+            queue_.push(std::make_unique<T>());
+        }
+    }
 }
 
 template <typename T>
 std::unique_ptr<T> ThreadSafeQueue<T>::get() {
-    return nullptr;
+    std::unique_ptr<T> res;
+    {
+        std::lock_guard<std::mutex> lockGuard(mutex_);
+        if (not queue_.empty()) {
+            res = std::move(queue_.front());
+            queue_.pop();
+        }
+    }
+
+    return std::move(res);
 }
 
 template <typename T>
-void ThreadSafeQueue<T>::set(std::unique_ptr<T> data) {
+void ThreadSafeQueue<T>::set(std::unique_ptr<T>&& data) {
+    if (data == nullptr) {
+#ifdef DEBUG
+        std::cerr << "set data can't be null";
+#endif
+        return;
+    }
 
+    if (queue_.size() + 1 > QUEUE_DATA_NUM) {
+        throw std::runtime_error("Queue size can't be greater QUEUE_DATA_NUM.");
+    }
+
+    std::lock_guard<std::mutex> lockGuard(mutex_);
+    queue_.push(std::move(data));
 }
 
+} // namespace network
 
 #endif  // NETWORKING_THREAD_SAFE_QUEUE_H
