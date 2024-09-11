d5c6e408d6da8f5658e12df23f187c9489c22cdb
Renat <renat.khatymov@plc2.de>
2024-09-11 10:32:02 +0300

    add script for testing

---
+++ scripts/integration_test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Get the directory of the script
+SCRIPT_DIR="$(dirname "$0")"
+
+# Convert to an absolute path
+ABSOLUTE_PATH="$(realpath "$SCRIPT_DIR")"
+#
+#BUILD_SCRIPT="${ABSOLUTE_PATH}/build.sh"
+#
+#chmod +x ${BUILD_SCRIPT}
+#${BUILD_SCRIPT}
+
+build_dir="cmake-build-debug"
+bin="./network"
+data_path="../data/callAlex.mp4"
+cd ${ABSOLUTE_PATH}/../${build_dir}
+#echo ${ABSOLUTE_PATH}/../build
+#echo Run server and a few client simultaniously
+
+#server
+set -x
+${bin} 127.0.0.1 1235 &
+# Sleep for 100 milliseconds (0.1 seconds)
+sleep 0.1
+#clients
+${bin} 127.0.0.1 1235 ${data_path} & ${bin} 127.0.0.1 1235 ${data_path}
