e24a6aa94097791ffd21fe6561981c28e452b16f
Rene <khatymovrenat@gmail.com>
2025-03-27 22:34:44 +0500

    Fix header orders

--- src/pch.h
+++ src/pch.h
@@ -1,20 +1,22 @@
 #pragma once
 
 #include <array>
-#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/asio/completion_condition.hpp>
+#include <boost/asio/ip/address.hpp>
 #include <cstdint>
 #include <cstdio>
 #include <filesystem>
 #include <fstream>
-#include <thread>
+#include <iostream>
 #include <memory>
 #include <queue>
 #include <string>
-#include "cryptopp/aes.h"
-#include "cryptopp/osrng.h"
-#include "cryptopp/modes.h"
+#include <thread>
+
 #include <openssl/sha.h>
-#include <boost/asio.hpp>
-#include <boost/asio/completion_condition.hpp>
-#include <boost/asio/ip/address.hpp>
+
+#include "cryptopp/aes.h"
+#include "cryptopp/modes.h"
+#include "cryptopp/osrng.h"
 #include "spdlog/spdlog.h"
